32a313fdd1dd8b4e330b6dfc828c9ac4cc7a886c
dnilks <40877619+dnilks@users.noreply.github.com>
2018-07-09 13:35:21 +0600

    Revert "Revert "add .gitignore.""
    This reverts commit f1ad542b66f7052f7865ac5253177b90706f8330.

---
+++ calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textNumber1 = new System.Windows.Forms.TextBox();
+            this.textNumber2 = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textNumber1
+            // 
+            this.textNumber1.Location = new System.Drawing.Point(13, 13);
+            this.textNumber1.Name = "textNumber1";
+            this.textNumber1.Size = new System.Drawing.Size(263, 26);
+            this.textNumber1.TabIndex = 0;
+            this.textNumber1.TextChanged += new System.EventHandler(this.textNumber1_TextChanged);
+            // 
+            // textNumber2
+            // 
+            this.textNumber2.Location = new System.Drawing.Point(345, 12);
+            this.textNumber2.Name = "textNumber2";
+            this.textNumber2.Size = new System.Drawing.Size(263, 26);
+            this.textNumber2.TabIndex = 1;
+            this.textNumber2.TextChanged += new System.EventHandler(this.textNumber2_TextChanged);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(13, 107);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(79, 61);
+            this.buttonPlus.TabIndex = 2;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(197, 107);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(79, 61);
+            this.buttonMinus.TabIndex = 3;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(345, 107);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(79, 61);
+            this.buttonMultiply.TabIndex = 4;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(529, 107);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(79, 61);
+            this.buttonDivide.TabIndex = 5;
+            this.buttonDivide.Text = "/";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(179, 55);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(263, 26);
+            this.textResult.TabIndex = 6;
+            this.textResult.TextChanged += new System.EventHandler(this.textResult_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 226);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.textNumber2);
+            this.Controls.Add(this.textNumber1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNumber1;
+        private System.Windows.Forms.TextBox textNumber2;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.TextBox textResult;
+    }
+}
+
---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double number1, number2, result;
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(textNumber1.Text);
+            number2 = Convert.ToDouble(textNumber2.Text);
+            result = number1 + number2;
+            textResult.Text = result.ToString();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(textNumber1.Text);
+            number2 = Convert.ToDouble(textNumber2.Text);
+            result = number1 - number2;
+            textResult.Text = result.ToString();
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(textNumber1.Text);
+            number2 = Convert.ToDouble(textNumber2.Text);
+            result = number1 * number2;
+            textResult.Text = result.ToString();
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(textNumber1.Text);
+            number2 = Convert.ToDouble(textNumber2.Text);
+            result = number1 / number2;
+            textResult.Text = result.ToString();
+        }
+
+        private void textResult_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void textNumber2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textNumber1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
